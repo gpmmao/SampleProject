9b751928cdab0934b34f5cb40b735e3e563b8c5e
gpmao <gpmao1@gmail.com>
2025-10-18 23:15:23 -0400

    fix null ref for annualSalary

--- WebApi/Models/Users/UserModel.cs
+++ WebApi/Models/Users/UserModel.cs
@@ -8,7 +8,7 @@ namespace WebApi.Models.Users
         public string Name { get; set; }
         public string Email { get; set; }
         public UserTypes Type { get; set; }
-        public decimal? AnnualSalary { get; set; }
+        public decimal? MonthlySalary { get; set; }
         public IEnumerable<string> Tags { get; set; }
     }
 }
